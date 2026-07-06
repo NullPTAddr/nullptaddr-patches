326701201560f1ee4a5c5b417fd19267979651e7
NullPTAddr <kang.rio123@gmail.com>
2026-07-07 05:23:46 +0700

    refactor: rename project and populate app names in constants

--- patches/src/main/kotlin/app/morphe/patches/maps/shared/Constants.kt
+++ patches/src/main/kotlin/app/morphe/patches/maps/shared/Constants.kt
@@ -5,6 +5,7 @@ import app.morphe.patcher.patch.Compatibility
 internal object Constants {
     const val PACKAGE_NAME = "com.google.android.apps.maps"
     val compatibility = Compatibility(
+        name = "Google Maps",
         packageName = PACKAGE_NAME
     )
 }
